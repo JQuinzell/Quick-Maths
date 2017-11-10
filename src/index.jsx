e098328f0c4fc19278a03d95b8876dec164fc5f1
MrRainwater <jqrvz6@mst.edu>
2017-11-10 14:59:28 -0600

    make important modules available as globals

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,11 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
 import LeastSquares from './hw_problems/LeastSquares'
+import math from 'mathjs'
+import * as ch6 from './math/ch6'
+
+window.math = math
+window.ch6 = ch6
 
 ReactDOM.render(
   <LeastSquares/>,
